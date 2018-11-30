e076281696940c761a0ab22f7eacf2412635cef6
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2018-11-30 08:49:53 -0500

    Update medium_quiz.js

--- quizzes/js/medium_quiz.js
+++ quizzes/js/medium_quiz.js
@@ -1,1 +1,85 @@
-
+var ans, i, len, x, z, y, counter=0, j= -1, n, num=0;
+var q=[
+        "Never change lanes in traffic without:",
+        "The world is round?"
+        ];
+var a=[
+        'Looking in the rear view mirror only', 
+        'hi'
+        ];
+var alen= a.length;
+var b=[
+        'Giving proper signal and looking to make sure the move can be made safely\n', 
+        'greetings'
+        ];
+var c=[
+        'Blowing your horn and looking to the rear', 
+        'bye'
+        ];
+var d=[
+        'Decreasing speed and giving correct signal', 
+        'hello\n'
+        ];
+function question() {
+j++;
+num++;
+document.getElementById("number").innerHTML = num + "/10 Questions";
+document.getElementById("que").innerHTML ='<p>'+ q[j] +'</p><br><form><input type="radio" name="choice" value="'+ a[j] +'">'+ a[j] +'<br><input type="radio" name="choice" value="'+ b[j] +'">'+ b[j] +'<br><input type="radio" name="choice" value="'+ c[j] +'">'+ c[j] +'<br><input type="radio" name="choice" value="'+ d[j] +'">'+ d[j] +'<br></form><button onclick="answer('+ j +')">Submit Answer</button><div id="correct"></div><div id="menu"></div>';
+}
+function answer(h) {
+  x ="";
+  ans = document.getElementsByName('choice');
+  len = ans.length;
+  for (i = 0; i < len; i++) {
+      if (ans[i].checked) {
+         x = ans[i].value; 
+         break;
+       }
+  }
+var all=[a[h], b[h], c[h], d[h]];
+for (y = 0; y < 4; y++){
+  n = all[y].endsWith("\n");
+  if (n == true){
+    n=all[y];
+    break;
+  }
+}
+  if (x == "") {
+   document.getElementById("correct").innerHTML = "PLease pick an answer";
+  }else if (x == n) {
+    counter++;
+   document.getElementById("correct").innerHTML = "Correct!";
+  } else {
+   document.getElementById("correct").innerHTML = "Incorrect! <br> The correct answer was: "+ n;
+  }
+if ((h+1) > (alen-1)){
+document.getElementById("menu").innerHTML =
+'<button onclick="end()">End of Quiz!</button>'
+}else{
+  document.getElementById("menu").innerHTML =
+'<button onclick="question()">Next Question!</button>'
+}
+}
+function end(){
+document.getElementById("number").innerHTML = "";
+    if (counter == 10){
+      document.getElementById("que").innerHTML = "Perfect! <br> You got "+ counter +"/10!";
+    }else if (counter > 7){
+      document.getElementById("que").innerHTML = "Awesome! <br> You got "+ counter +"/10!";
+    }else if (counter > 5){
+      document.getElementById("que").innerHTML = "Practice Makes Perfect! You got "+ counter +"/10!";
+    }else if (counter > 3){
+      document.getElementById("que").innerHTML = "Good Try!  <br> You got "+counter +"/10!";
+   }else{
+      document.getElementById("que").innerHTML = "More practice may be needed. <br> You got "+ counter +"/10!";
+   }
+document.getElementById("que").innerHTML +=
+'<br><button onclick= "retry()">Retry!</button>'
+document.getElementById("que").innerHTML +=
+'<br><a href="Hard_Quiz.html">Try the Hard Quiz!</a>'
+}
+function retry(){
+j=-1;
+counter=0;
+question();
+}
