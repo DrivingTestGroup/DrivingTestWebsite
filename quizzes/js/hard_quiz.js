006c8cc87bfb442a4b6b5d4050bfd3d4f5074f8b
AbigailDenney <42846795+AbigailDenney@users.noreply.github.com>
2018-12-06 09:01:17 -0500

    Update hard_quiz.js

--- quizzes/js/hard_quiz.js
+++ quizzes/js/hard_quiz.js
@@ -3,28 +3,33 @@ var ans, i, len, x, z, y, counter=0, j= -1, n, num=0;
 var q=[
         "How soon after a licensed driver changes his/her name or address is he/she required to notify the Ministry of Transportation and Communications?",
         "If you are convicted of careless driving, you will get six demerit points and can get:",
-        "When changing lanes on a multi-lane highway, a driver should:"
+        "When changing lanes on a multi-lane highway, a driver should:",
+        "When lights are required, drivers must use low-beam headlights:"
         ];
 var a=[
         "Within 15 days.",
         'Your license suspened for up to two years.',
-        'Activate emergency signals.'
+        'Activate emergency signals.',
+        "Within 150m of an oncoming vehicle.\n"
         ];
 var alen= a.length;
 var b=[
         'Within 6 days.\n', 
         'A fine up to 2 years',
-        'Honk at other drivers.'
+        'Honk at other drivers.',
+        "Within 300m of an oncoming vehicle."
         ];
 var c=[
         'Within 30 days.', 
         'A jail sentence up to six months.',
-        'Check mirors and signals before moving left or right.\n'
+        'Check mirors and signals before moving left or right.\n',
+        "At their discretion. This is a safety practice, not a law."
         ];
 var d=[
         'Any time prior to the renewal of the license.',
         'All of the above.\n',
-        'Not concern themselves with the other drivers.'
+        'Not concern themselves with the other drivers.',
+        "Whith 1km of an oncoming vehicle."
         ];
 function question() {
 j++;
