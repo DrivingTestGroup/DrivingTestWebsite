b764ce3cbac82c7ecf67afbb2fd5f6c2f7386a6f
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2018-12-25 03:30:40 -0500

    Create Medium_Quiz.php

---
+++ quizzes/Medium_Quiz.php
@@ -0,0 +1,25 @@
+<?php
+include '../navigation_bar/navigation.php';
+?>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="style.css" rel="stylesheet" type="text/css" />
+  <link href="../navigation_bar/navigation_styles.css" rel="stylesheet" type="text/css" />
+  <script type="text/javascript" src="js/medium_quiz.js"></script>
+  </head>
+  <body>
+    <p id="number" style="float: right;"></p>
+    <div class="container-fluid" id="que">
+    <h>Medium Quiz!</h>
+    <br>
+    <button onclick='question()'>Start</button>
+
+<div id="correct"></div>
+<div id="menu"></div>
+
+</div>
+</body>
+</html>
