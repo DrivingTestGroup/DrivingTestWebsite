3d348238bce722d309380edc21e361f9e186f590
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2018-12-25 03:21:57 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+include 'navigation_bar/navigation.php';
+?>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link href="website_styles.css" rel="stylesheet" type="text/css" />
+    <link href="navigation_bar/navigation_styles.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+
+  <div class="home_image">
+    <div class="cnt_head_home_img"> 
+      <h2 class="header"> All Roads Begin Here.
+ Start learning how to drive today! </h2>
+    </div>
+  </div>
+
+  <div class="description">
+    <h2> Start learning how to drive today!</h2>
+    <p>This is a practical guide that will prepare you for real G1 driving test. Based off the Official Ministry of Transportation (MTO) Driver's Handbook you will find information that will give you a good idea of what to expect on the test. This free program will allow you to learn at your own pace on any device. Buckle up your seat beats, this is going to be a thrilling ride!
+
+    </p>
+  </div>
+
+  <div class="mv_img2">
+    <div>
+      <h2> So… What should you do to ensure you get passed on your first try? </h2>
+      <p> 
+        Looking under our info page will give you the essential information you need to know before doing your G1 Driving Test. After, you should test your own knowledge by trying our quizzes that range from beginner to advanced, allowing you to get help through every step of the way. Even trying our mini games will give you a practical idea of how to apply the information you learned to a real world experience.  Once you’ve prepared sufficiently, it’s time to book your appointment at the DriveTest Centre and pass with flying colours.  
+      </p>
+    </div>
+  </div>
+
+
+  <div class="footer">
+    <div class="footer_items">
+      <p>footer content (creators, link, etc.)</p>
+    </div>
+  </div>
+  
+  </body>
+</html>
