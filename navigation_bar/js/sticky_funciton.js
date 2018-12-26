a0b2d033e47afa42f89f9f0cc485948ae69a20de
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2018-12-26 17:02:31 -0500

    Update sticky_funciton.js

--- navigation_bar/js/sticky_funciton.js
+++ navigation_bar/js/sticky_funciton.js
@@ -5,9 +5,17 @@ window.onscroll = function(){stick()};
 //variable for the navigation bar
 var navigationBar = document.getElementById("stickyNavbar");
 
+//variable for the extra small navigation bar
+var xsNavbar = document.getElementById("stickyXsNavbar");
+
+
+
 
 //variable for the offset of the navigation bar from the top of the page
 var navigationOffset = navigationBar.offsetTop;
+
+//variable for the offset of the extra small navigation bar from the top of the page
+var xsNavbarOffset = xsNavbar.offsetTop;
 
 //function for setting sticky position on the navigation page
 function stick() {
@@ -15,5 +23,13 @@ function stick() {
     navigationBar.classList.add("sticky");
   } else {
     navigationBar.classList.remove("sticky");
+  }
+  
+  
+    //make the contacts button stick to the top of the page
+  if (window.pageYOffset > xsNavbarOffset) {
+    xsNavbar.classList.add("sticky");
+  } else {
+    xsNavbar.classList.remove("sticky");
   }
 }
