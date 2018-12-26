af050f192222f642010f73a7631a07937bb6003d
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2018-12-26 05:29:35 -0500

    Update navigation.php
    made responsive navigation bar

--- navigation_bar/navigation.php
+++ navigation_bar/navigation.php
@@ -10,14 +10,99 @@
   <div class="title">
     <h2 class="title-heading">G1 Driving Test</h2>
   </div>
+  
+  
+  <div class="xs-menu" id="stickyNavbar">
+    <div class="menu_icon">
+      <div class="hamburger_line">
+      </div>
+      <div class="hamburger_line">
+      </div>
+      <div class="hamburger_line">
+      </div>
+      
+          <div class="xs_navbar">
+        <a class="xs_menu_link" href="index.html"> HOME 
+          <div class="underline">
+          </div>
+        </a>
+      
+      <div class="xs_dropdown">
+        <div class="xs_dropdown_button">INFO
+          <div class="underline">
+          </div>
+        </div>
+          <div class="xs_dropdown_content">
+                      <a class="dropdown-selection" href="info_pages/introduction.html"> What is the G1 Test? </a>
 
+          <a class="xs_dropdown-selection" href="info_pages/How_to_get_it.html "> How to get it </a>
+
+          <a class="xs_dropdown-selection" href="info_pages/Ontario_Drivers_Handbook.html">Ontario Drivers Handbook </a>
+
+          <a class="xs_dropdown-selection" href="info_pages/When_and_Where.html"> When and Where</a>
+
+          <a class="xs_dropdown-selection" href="info_pages/More_Information.html"> More Information </a>
+          </div>
+      </div>
+      
+      
+      <div class="xs_dropdown">
+        <div class="xs_dropdown_button">QUIZZES
+          <div class="underline">
+          </div>
+        </div>
+        
+                <div class="xs_dropdown_content">
+          <a  href="https://wwwg1drivingtestwebsitetk.000webhostapp.com/quizzes/easy_quiz.php">Easy </a>
+
+          <a  href="https://wwwg1drivingtestwebsitetk.000webhostapp.com/quizzes/Medium_Quiz.php">Medium </a>
+
+          <a  href="https://wwwg1drivingtestwebsitetk.000webhostapp.com/quizzes/Hard_Quiz.php">Hard </a>
+        </div>
+      </div>
+      
+      
+       <div class="xs_dropdown">
+        <div class="xs_dropdown_button">MINIGAMES
+          <div class="underline">
+          </div>
+        </div>
+         
+        <div class="xs_dropdown_content">
+          <a href="games/game1.html">Game A</a>
+
+          <a href="games/game2.html">Game B</a>
+
+          <a href="games/game3.html">Game C</a>
+        </div>
+      </div>
+      
+      <a class="xs_menu_link" href="index.html"> CONTACTS
+        <div class="underline">
+        </div>
+      </a>
+    </div>
+    </div>
+    
+    
+
+  </div>
+  
+ 
+  
 
   <div class="navbar" id="stickyNavbar">
-    <a class="menu-button" href="https://wwwg1drivingtestwebsitetk.000webhostapp.com/"> HOME </a>
+    <div class="menu-button">
+    <a class="menu-link" href="index.html"> HOME 
+      <div class="underline">
+    </div></a>
+      
 
 
    <div class="dropdown">
      <div class="dropdown-button">INFO
+       <div class="underline">
+       </div>
         <i class="fa fa-caret-down"></i>
       </div>
         <div class="dropdown-content">
@@ -35,11 +120,13 @@
 
 
       <div class="dropdown">
-        <div class="dropdown-button">QUIZZES
+        <div class="dropdown-button" >QUIZZES
+          <div class="underline">
+          </div>
           <i class="fa fa-caret-down"></i>
         </div>
         <div class="dropdown-content">
-          <a  href="https://wwwg1drivingtestwebsitetk.000webhostapp.com/quizzes/Easy_Quiz.php">Easy </a>
+          <a  href="https://wwwg1drivingtestwebsitetk.000webhostapp.com/quizzes/easy_quiz.php">Easy </a>
 
           <a  href="https://wwwg1drivingtestwebsitetk.000webhostapp.com/quizzes/Medium_Quiz.php">Medium </a>
 
@@ -50,6 +137,8 @@
 
       <div class="dropdown">
         <div class="dropdown-button">MINIGAMES
+          <div class="underline">
+          </div>
           <i class="fa fa-caret-down"></i>
         </div>
         <div class="dropdown-content">
@@ -63,8 +152,12 @@
     </div>
 
 
-    <a class="menu-button" href="index.html"> CONTACTS </a>
-    </div>
+    <a class="menu-link" href="index.html"> CONTACTS
+      <div class="underline">
+      </div> </a>
+  </div>
+  </div>
+    
     
     <script type="text/javascript" src="https://wwwg1drivingtestwebsitetk.000webhostapp.com/navigation_bar/js/sticky_funciton.js"></script>
     
