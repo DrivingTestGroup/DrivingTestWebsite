c335d9b11ea496767115ed65800207f56f540522
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-01-02 13:47:27 -0500

    Update Hard_Quiz.php

--- quizzes/Hard_Quiz.php
+++ quizzes/Hard_Quiz.php
@@ -7,19 +7,23 @@ include '../navigation_bar/navigation.php';
   <head>
     <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="style.css" rel="stylesheet" type="text/css" />
+  <link href="quiz_style.css" rel="stylesheet" type="text/css" />
   <link href="../navigation_bar/navigation_styles.css" rel="stylesheet" type="text/css" />
   <script src="js/hard_quiz.js"></script>
   </head>
   <body>
-    <p id="number" style="float: right;"></p>
-    <div class="container-fluid" id="que">
-    <h>Hard Quiz!</h>
-    <br>
-    <button onclick='question()'>Start</button>
+  <div class="background">  
+    <div class="quiz_container">
+      <p class="counter" id="number"></p>
+      <div class="quiz" id="que">
+        <h>Hard Quiz!</h>
+        <br>
+        <button class="start_end"onclick='question()'>Start</button>
 
-<div id="correct"></div>
-<div id="menu"></div>
-
-</div>
+      <div id="correct"></div>
+      <div id="menu"></div>
+      </div>
+    </div>
+  </div>
 </body>
+</html>
