112ba9f73a158718cad171fbb81a4b015b8cd4fd
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-01-24 21:03:41 -0500

    Create road_selector.php

---
+++ games/road_selector.php
@@ -0,0 +1,57 @@
+<?php
+include '../navigation_bar/navigation.php';
+?>
+
+<html>
+
+  <head>
+
+    <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="../quizzes/quiz_style.css" rel="stylesheet" type="text/css" />
+
+  <script type="text/javascript" src="js/road_selector.js"></script>
+
+  </head>
+
+  <body>
+
+    <div class="background medium_start_background" id="background">  
+
+    <div class="quiz_container">
+
+      <p class="counter" id="number"></p>
+
+      <div id="game"></div>
+
+      <div class="quiz" id="que">
+
+        <h>Minigames!</h>
+
+        <p>Watch the video then chose the correct answer!</p>
+
+        <br>
+
+        <button class="start_end" onclick='question()'>Start</button>
+
+        
+
+      <div id="correct"></div>
+
+      <div id="menu"></div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</body>
+
+</html>
+
+<?php
+include '../footer/footer.php';
+?>
