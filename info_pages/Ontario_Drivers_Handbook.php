4947f5a8a4c0804dc3a8540d85a64c72d9f363e6
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-01-24 21:09:21 -0500

    Create Ontario_Drivers_Handbook.php

---
+++ info_pages/Ontario_Drivers_Handbook.php
@@ -0,0 +1,32 @@
+<?php
+include '../navigation_bar/navigation.php';
+?>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="Ontario_Drivers_Handbook.css">
+  </head>
+  <body>
+      <div class="content">
+      <div class="paragraph_box">
+      <h1>Ontario Driver's Handbook</h1>
+
+    <div class="paragraph"> The Driver’s Handbook can be found online on the official MTO website, a complete printed copy can be ordered from 
+  ServiceOntario ($14.95), or bought from your local Canadian Tire. Although it can be difficult to keep track of all the 
+  information in the Driver’s Handbook. Our practice tests and mini-games are a great way to help you integrate that knowledge 
+  and reinforce it without re-reading the same dull material.</div>
+
+
+    <a class='info_link' href='introduction.html'>What is the G1 test?</a><br>
+    <a class='info_link' href='More_Information.html'>FAQ</a>
+    </div>
+    </div>
+</body>
+</html>
+
+
+<?php
+include '../footer/footer.php';
+?>
