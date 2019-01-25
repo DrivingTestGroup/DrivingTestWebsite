1f3f4257e4414bf1d910d9cc79dde619bcfbc5d3
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-01-24 21:28:08 -0500

    Create insert.php

---
+++ database/insert.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "id8147643_root";
+$password = "Iscream567";
+$db = "id8147643_g1drivingtest";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully<br>";
+?>
